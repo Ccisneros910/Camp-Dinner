9f77e8583fe13cfa4e76ab5c9561541cf9ccce90
Carlos Cisneros <33742776+Ccisneros910@users.noreply.github.com>
2020-07-10 23:07:30 -0700

    Controls and Collision
    - Player can move their character around the map in an 8-directional manner
    - Player collision with bushes recognized

---
+++ Happy Campers/Assets/PlayerCollision.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "Bush")
+        {
+            Debug.Log("Bush");
+        }
+    }
+}
